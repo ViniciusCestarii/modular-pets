e63d0d3d0ba01244e13b8a14fb280e37a37eb76c
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-11-29 15:35:43 -0300

    feat: add automatic build and lint check

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -10,5 +9,4 @@ export default [
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintPluginPrettierRecommended,
 ];
