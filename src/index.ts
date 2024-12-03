b8d72f36f6f4d9562b2a103f39e6b29c3f1b61b9
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-12-03 14:39:56 -0300

    feat: add swagger

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import { Elysia } from "elysia";
 import { env } from "./env";
 import petsRoutes from "./modules/pet/pets/routes";
 import speciesRoutes from "./modules/pet/species/routes";
+import swagger from "@elysiajs/swagger";
 
-const app = new Elysia().use(petsRoutes).use(speciesRoutes).listen(env.PORT);
-
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-);
+const app = new Elysia()
+  .use(swagger())
+  .use(petsRoutes)
+  .use(speciesRoutes)
+  .listen(env.PORT);
 
 export default app;
