29fa5cdc33deda329df4fc2f6ce73cde5dbef7c9
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-18 08:16:26 -0300

    refact: env to path to keys must be explicit set

--- src/env/index.ts
+++ src/env/index.ts
@@ -5,8 +5,8 @@ import { Value } from "@sinclair/typebox/value";
 const envSchema = t.Object({
   PORT: t.String({ default: "3333" }),
   DATABASE_URL: t.String({ format: "uri" }),
-  PUBLIC_TOKEN_KEY_PATH: t.String({ default: "keys/test/token-public.pem" }),
-  PRIVATE_TOKEN_KEY_PATH: t.String({ default: "keys/test/token-private.pem" }),
+  PUBLIC_TOKEN_KEY_PATH: t.String(),
+  PRIVATE_TOKEN_KEY_PATH: t.String(),
   AXIOM_DATASET: t.Optional(t.String()),
   AXIOM_TOKEN: t.Optional(t.String()),
 });
