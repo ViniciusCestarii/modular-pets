d86870b99a179255e93d5e85624af9f8c4b30a66
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-10-27 21:07:20 -0300

    fix: delete pet first then image

--- src/modules/pet/pets/repositories/drizzle-repository.ts
+++ src/modules/pet/pets/repositories/drizzle-repository.ts
@@ -188,12 +188,12 @@ export class DrizzlePetsRepository implements PetsRepository {
 
   deletePet(petId: Pet["id"]): Promise<void> {
     return db.transaction(async (tx) => {
+      await tx.delete(petsTable).where(eq(petsTable.id, petId));
       await tx
         .delete(imagesTable)
         .where(
           and(eq(imagesTable.ownerId, petId), eq(imagesTable.ownerType, "pet")),
         );
-      await tx.delete(petsTable).where(eq(petsTable.id, petId));
     });
   }
 }
